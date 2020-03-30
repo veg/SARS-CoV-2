126093770a24a8e7b7369c3aa45e23559f469395
Steven Weaver <sweaver@temple.edu>
2020-03-30 14:31:58 -0400

    pipeline modifications

--- scripts/extract_genes.sh
+++ scripts/extract_genes.sh
@@ -1,10 +1,31 @@
-FILE=$1
-NP=6
-HYPHY=/usr/local/bin/hyphy
-HYPHYMPI=/usr/local/bin/HYPHYMPI
+#!/bin/bash
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=99:0:0:0
+
+export PATH=/usr/local/bin:$PATH
+source /etc/profile.d/modules.sh
+module load aocc/1.3.0
+module load openmpi/gnu/3.0.2
+
+DIRECTORY=$1
+FILE=$1/sequences
+ATTRIBUTES=$1/attributes.csv
+MASTER=$1/master.json
+MASTERNOFASTA=$1/master-no-fasta.json
+GENE=$2
+NP=16
+HYPHY=/data/shares/veg/SARS-CoV-2/hyphy/hyphy
+HYPHYMPI=/data/shares/veg/SARS-CoV-2/hyphy/HYPHYMPI
+HYPHYLIBPATH=/data/shares/veg/SARS-CoV-2/hyphy/res
 MAFFT=/usr/local/bin/mafft
 RAXML=/usr/local/bin/raxml-ng
 TN93=/usr/local/bin/tn93
+#PREMSA=/Users/sergei/Development/hyphy-analyses/codon-msa/pre-msa.bf
+#POSTMSA=/Users/sergei/Development/hyphy-analyses/codon-msa/post-msa.bf
+PREMSA=/data/shares/veg/SARS-CoV-2/hyphy-analyses/codon-msa/pre-msa.bf
+POSTMSA=/data/shares/veg/SARS-CoV-2/hyphy-analyses/codon-msa/post-msa.bf
+WORKING_DIR=/data/shares/veg/SARS-CoV-2/SARS-CoV-2/
+
 
 function run_a_gene {
 
@@ -24,9 +45,12 @@ else
     then
         echo "Already extracted"
     else
-        mpirun -np $NP $HYPHYMPI  /Users/sergei/Development/hyphy-analyses/codon-msa/pre-msa.bf --input $FILE --reference $REFERENCE_SEQUENCE --trim-from $TRIM_FROM --trim-to $TRIM_TO --N-fraction $N_FRAC
-        mv ${FILE}_protein.fas ${FILE}.${GENE}_protein.fas
-        mv ${FILE}_nuc.fas ${FILE}.${GENE}_nuc.fas
+        TMP_FILE=${FILE}.${GENE}.tmp
+        cp ${FILE} ${TMP_FILE}
+        echo "mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH $PREMSA --input $TMP_FILE --reference $REFERENCE_SEQUENCE --trim-from $TRIM_FROM --trim-to $TRIM_TO --N-fraction $N_FRAC"
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH $PREMSA --input $TMP_FILE --reference $REFERENCE_SEQUENCE --trim-from $TRIM_FROM --trim-to $TRIM_TO --N-fraction $N_FRAC
+        mv ${TMP_FILE}_protein.fas ${FILE}.${GENE}_protein.fas
+        mv ${TMP_FILE}_nuc.fas ${FILE}.${GENE}_nuc.fas
     fi
     
     echo "ALIGNING PROTEIN DATA"
@@ -34,31 +58,33 @@ else
     then
         echo "Already aligned"
     else
-        $MAFFT ${FILE}.${GENE}_protein.fas > ${FILE}.${GENE}.msa
+        echo $MAFFT ${FILE}.${GENE}_protein.fas > ${FILE}.${GENE}.msa
+        $MAFFT ${FILE}.${GENE}_protein.fas > ${FILE}.${GENE}.msa 2> mafft.error.log
     fi
         
     if [ -s ${FILE}.${GENE}.compressed.fas ] 
     then
         echo "Already reverse translated"
     else
-        $HYPHY /Users/sergei/Development/hyphy-analyses/codon-msa/post-msa.bf --protein-msa ${FILE}.${GENE}.msa --nucleotide-sequences ${FILE}.${GENE}_nuc.fas --output ${FILE}.${GENE}.compressed.fas --duplicates ${FILE}.${GENE}.duplicates.json
-        $HYPHY /Users/sergei/Development/hyphy-analyses/codon-msa/post-msa.bf --protein-msa ${FILE}.${GENE}.msa --nucleotide-sequences ${FILE}.${GENE}_nuc.fas --compress No --output ${FILE}.${GENE}.all.fas    
+        $HYPHY LIBPATH=$HYPHYLIBPATH $POSTMSA --protein-msa ${FILE}.${GENE}.msa --nucleotide-sequences ${FILE}.${GENE}_nuc.fas --output ${FILE}.${GENE}.compressed.fas --duplicates ${FILE}.${GENE}.duplicates.json
+        $HYPHY LIBPATH=$HYPHYLIBPATH $POSTMSA --protein-msa ${FILE}.${GENE}.msa --nucleotide-sequences ${FILE}.${GENE}_nuc.fas --compress No --output ${FILE}.${GENE}.all.fas    
     fi
     
-     
     if [ -s ${FILE}.${GENE}.withref.fas ]
     then 
         echo "Already has alignment with reference"
     else
-        $MAFFT --add $REFERENCE_SEQUENCE --reorder ${FILE}.${GENE}.all.fas > ${FILE}.${GENE}.withref.fas
+        echo $MAFFT --add $REFERENCE_SEQUENCE --reorder ${FILE}.${GENE}.all.fas > ${FILE}.${GENE}.withref.fas
+        $MAFFT --add $REFERENCE_SEQUENCE --reorder ${FILE}.${GENE}.all.fas > ${FILE}.${GENE}.withref.fas 2> mafft.error.log
     fi 
 
     if [ -s ${FILE}.${GENE}.tn93 ] 
     then
         echo "Already computed TN93"
     else
-        $TN93 -q -t 0.05 ${FILE}.${GENE}.withref.fas > ${FILE}.${GENE}.tn93 2> ${FILE}.${GENE}.tn93.json
-        python3 python/tabulate-diversity-divergence.py -j data/db/master.json -t ${FILE}.${GENE}.tn93 > data/evolution.${GENE}.csv
+        $TN93 -q -t 0.05 $WORKING_DIR/${FILE}.${GENE}.withref.fas > ${FILE}.${GENE}.tn93 2> ${FILE}.${GENE}.tn93.json
+        echo 'python3 $WORKING_DIR/python/tabulate-diversity-divergence.py -j $MASTER -t ${FILE}.${GENE}.tn93 > $WORKING_DIR/data/evolution.${GENE}.csv'
+        python3 $WORKING_DIR/python/tabulate-diversity-divergence.py -j $MASTER -t ${FILE}.${GENE}.tn93 > $WORKING_DIR/data/evolution.${GENE}.csv
     fi
 
     if [ -s ${FILE}.${GENE}.compressed.fas.raxml.bestTree ] 
@@ -68,82 +94,116 @@ else
         $RAXML --msa ${FILE}.${GENE}.compressed.fas --model GTR+G --force
     fi
     
-    if [ -s ${FILE}.${GENE}.SLAC.json ] 
+    if [ -s $WORKING_DIR/${FILE}.${GENE}.SLAC.json ] 
     then
         echo "Already has SLAC results"
     else
-        mpirun -np $NP $HYPHYMPI slac --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.SLAC.json
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH slac --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.SLAC.json
     fi
 
     if [ -s ${FILE}.${GENE}.FEL.json ] 
     then
         echo "Already has FEL results"
     else
-        mpirun -np $NP $HYPHYMPI fel --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.FEL.json
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH fel --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.FEL.json
     fi
 
     if [ -s ${FILE}.${GENE}.MEME.json ] 
     then
         echo "Already has MEME results"
     else
-        mpirun -np $NP $HYPHYMPI meme --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.MEME.json
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH meme --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.MEME.json
     fi
     
     if [ -s ${FILE}.${GENE}.PRIME.json ] 
     then
         echo "Already has PRIME results"
     else
-        mpirun -np $NP $HYPHYMPI prime --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.PRIME.json
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH prime --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.PRIME.json
     fi
-    
-    python3 python/summarize-gene.py -D data/db/master-no-fasta.json -d ${FILE}.${GENE}.duplicates.json -s ${FILE}.${GENE}.SLAC.json -f ${FILE}.${GENE}.FEL.json -m ${FILE}.${GENE}.MEME.json -P 0.1 -p ${FILE}.${GENE}.PRIME.json --output  ${FILE}.${GENE}.json -c ${FILE}.${GENE}.withref.fas
 
-    #if [ -s ${FILE}.${GENE}.BGM.json ] 
-    #then
-    #    echo "Already has BGM results"
-    #else
-    #    mpirun -np $NP $HYPHYMPI bgm --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --min-subs 2 --type codon
-    #fi
+    if [ -s ${FILE}.${GENE}.BGM.json ] 
+    then
+        echo "Already has BGM results"
+    else
+        mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH bgm --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --min-subs 2 --type codon
+    fi
 
-    #if [ -s ${FILE}.${GENE}.FADE.json ] 
-    #then
-    #    echo "Already has FADE results"
-    #else
-    #    $HYPHY scripts/reroot-on-oldest.bf --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --csv data/attributes.csv --output ${FILE}.${GENE}.compressed.fas.rooted
-    #    $HYPHY conv Universal "Keep Deletions" ${FILE}.${GENE}.compressed.fas  ${FILE}.${GENE}.compressed.fas.prot
-    #    $HYPHY fade --alignment ${FILE}.${GENE}.compressed.fas.prot --tree ${FILE}.${GENE}.compressed.fas.rooted --branches Internal
-    #    mpirun -np $NP $HYPHYMPI bgm --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --min-subs 2 --type codon
-    #fi
-
-
-    #if [ -s ${FILE}.${GENE}.BUSTED.json ] 
-    #then
-    #    echo "Already has BUSTED results"
-    #else
-    #    $HYPHY busted --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.BUSTED.json --rates 2 --syn-rates 2 --starting-points 10
-    #fi
-
+    if [ -s ${FILE}.${GENE}.BUSTED.json ] 
+    then
+        echo "Already has BUSTED results"
+    else
+        $HYPHY LIBPATH=$HYPHYLIBPATH busted --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.BUSTED.json --rates 2 --syn-rates 2 --starting-points 10
+    fi
 
     #if [ -s ${FILE}.${GENE}.ABSREL.json ] 
     #then
     #    echo "Already has ABSREL results"
     #else
-    #    mpirun -np $NP $HYPHYMPI absrel --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.ABSREL.json
+    #    mpirun -np $NP $HYPHYMPI LIBPATH=$HYPHYLIBPATH absrel --alignment ${FILE}.${GENE}.compressed.fas --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --branches Internal --output ${FILE}.${GENE}.ABSREL.json
     #fi
     
+    if [ -s ${FILE}.${GENE}.FADE.json ] 
+    then
+        echo "Already has FADE results"
+    else
+        echo $HYPHY LIBPATH=$HYPHYLIBPATH scripts/reroot-on-oldest.bf --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --csv $ATTRIBUTES --output ${FILE}.${GENE}.compressed.fas.rooted
+        $HYPHY LIBPATH=$HYPHYLIBPATH scripts/reroot-on-oldest.bf --tree ${FILE}.${GENE}.compressed.fas.raxml.bestTree --csv $ATTRIBUTES --output ${FILE}.${GENE}.compressed.fas.rooted
+        echo $HYPHY LIBPATH=$HYPHYLIBPATH conv Universal "Keep Deletions" ${FILE}.${GENE}.compressed.fas  ${FILE}.${GENE}.compressed.fas.prot
+        $HYPHY LIBPATH=$HYPHYLIBPATH conv Universal "Keep Deletions" ${FILE}.${GENE}.compressed.fas  ${FILE}.${GENE}.compressed.fas.prot
+        echo $HYPHY LIBPATH=$HYPHYLIBPATH fade --alignment ${FILE}.${GENE}.compressed.fas.prot --tree ${FILE}.${GENE}.compressed.fas.rooted --branches Internal
+        $HYPHY LIBPATH=$HYPHYLIBPATH fade --alignment ${FILE}.${GENE}.compressed.fas.prot --tree ${FILE}.${GENE}.compressed.fas.rooted --branches Internal
+    fi
+
+    python3 $WORKING_DIR/python/summarize-gene.py -D $MASTERNOFASTA -d ${FILE}.${GENE}.duplicates.json -s ${FILE}.${GENE}.SLAC.json -f ${FILE}.${GENE}.FEL.json -m ${FILE}.${GENE}.MEME.json -P 0.1 -p ${FILE}.${GENE}.PRIME.json --output  ${FILE}.${GENE}.json -c ${FILE}.${GENE}.withref.fas
     
 fi
 
 }
 
-run_a_gene "S" "data/reference_genes/S.fas" "20000" "27000" 0.005
-run_a_gene "M" "data/reference_genes/M.fas" "25000" "30000" 0.01
-run_a_gene "N" "data/reference_genes/N.fas" "26000" "35000" 0.01
-run_a_gene "ORF3a" "data/reference_genes/ORF3a.fas" "24000" "27000" 0.05
-run_a_gene "ORF6" "data/reference_genes/ORF6.fas" "26000" "30000" 0.05
-run_a_gene "ORF7a" "data/reference_genes/ORF7a.fas" "26000" "35000" 0.05
-run_a_gene "ORF8" "data/reference_genes/ORF8.fas" "26000" "35000" 0.05
-run_a_gene "ORF1a" "data/reference_genes/ORF1a.fas" "1" "15000" 0.001
-run_a_gene "ORF1b" "data/reference_genes/ORF1b.fas" "12000" "24000" 0.001
+case $GENE in
 
+  S)
+    echo -n "Analyzing S Gene"
+    run_a_gene "S" "$WORKING_DIR/data/reference_genes/S.fas" "20000" "27000" 0.005
+    ;;
 
+  M)
+    echo -n "Analyzing M gene"
+    run_a_gene "M" "$WORKING_DIR/data/reference_genes/M.fas" "25000" "30000" 0.01
+    ;;
+
+  N)
+    echo -n "Analyzing N gene"
+    run_a_gene "N" "$WORKING_DIR/data/reference_genes/N.fas" "26000" "35000" 0.01
+    ;;
+
+  ORF3a)
+    echo -n "Analyzing ORF3a gene"
+    run_a_gene "ORF3a" "$WORKING_DIR/data/reference_genes/ORF3a.fas" "24000" "27000" 0.05
+    ;;
+
+  ORF7a)
+    echo -n "Analyzing ORF7a gene"
+    run_a_gene "ORF7a" "$WORKING_DIR/data/reference_genes/ORF7a.fas" "26000" "35000" 0.05
+    ;;
+
+  ORF8)
+    echo -n "Analyzing ORF8 gene"
+    run_a_gene "ORF8" "$WORKING_DIR/data/reference_genes/ORF8.fas" "26000" "35000" 0.05
+    ;;
+
+  ORF1a)
+    echo -n "Analyzing ORF1a gene"
+    run_a_gene "ORF1a" "$WORKING_DIR/data/reference_genes/ORF1a.fas" "1" "15000" 0.001
+    ;;
+
+  ORF1b)
+    echo -n "Analyzing ORF1b gene"
+    run_a_gene "ORF1b" "$WORKING_DIR/data/reference_genes/ORF1b.fas" "12000" "24000" 0.001
+    ;;
+
+  *)
+    echo -n "Unknown Gene, exiting"
+    ;;
+esac
