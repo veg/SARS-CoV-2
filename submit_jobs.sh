ec896d5cc46dee7b04133c345064875f6c74ba6d
Steven Weaver <sweaver@temple.edu>
2020-04-15 13:01:04 -0400

    2020-04-15

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#fdate=$(date +"%Y-%m-%d")
-fdate="2020-04-13"
+fdate=$(date +"%Y-%m-%d")
+#fdate="2020-04-13"
 
 qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
 qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1b" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
