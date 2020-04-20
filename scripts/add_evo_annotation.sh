8a09e95e9790d2076afd92503ac5a2b2938a9303
Segei L Kosakovsky Pond <spond@temple.edu>
2020-04-19 21:43:45 -0400

    Removing incorrect RdRp evo annotation

--- scripts/add_evo_annotation.sh
+++ scripts/add_evo_annotation.sh
@@ -10,7 +10,7 @@ python3 python/extract-prime-annotation.py  -c data/betas/sequences.nsp8.compres
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.nsp9.compressed.fas -p data/betas/nsp9.PRIME.json -r ORF1a  -f 4140 -o data/evo_annotation.json 
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.nsp10.compressed.fas -p data/betas/nsp10.PRIME.json -r ORF1a  -f 4253 -o data/evo_annotation.json 
 # 
-python3 python/extract-prime-annotation.py  -c data/betas/sequences.RdRp.compressed.fas -p data/betas/RdRp.PRIME.json -r ORF1b  -f -10 -o data/evo_annotation.json 
+#python3 python/extract-prime-annotation.py  -c data/betas/sequences.RdRp.compressed.fas -p data/betas/RdRp.PRIME.json -r ORF1b  -f -10 -o data/evo_annotation.json 
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.helicase.compressed.fas -p data/betas/helicase.PRIME.json -r ORF1b  -f 922 -o data/evo_annotation.json 
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.endornase.compressed.fas -p data/betas/endornase.PRIME.json -r ORF1b  -f 2050 -o data/evo_annotation.json 
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.methyltransferase.compressed.fas -p data/betas/methyltransferase.PRIME.json -r ORF1b  -f 2396 -o data/evo_annotation.json 
@@ -20,3 +20,6 @@ python3 python/extract-prime-annotation.py  -c data/betas/S.aligned.fas -p data/
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.ORF6.compressed.fas -p data/betas/ORF6.PRIME.json -r ORF6 -f 0 -o data/evo_annotation.json
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.ORF8.compressed.fas -p data/betas/ORF8.PRIME.json -r ORF8 -f 0 -o data/evo_annotation.json
 python3 python/extract-prime-annotation.py  -c data/betas/sequences.ORF7a.compressed.fas -p data/betas/ORF7a.PRIME.json -r ORF7a -f 0 -o data/evo_annotation.json
+
+python3 python/extract-prime-annotation.py  -c data/betas/sequences.N.compressed.fas -p data/betas/N.PRIME.json -r N -f 0 -o data/evo_annotation.json
+python3 python/extract-prime-annotation.py  -c data/betas/sequences.M.compressed.fas -p data/betas/M.PRIME.json -r M -f 0 -o data/evo_annotation.json
