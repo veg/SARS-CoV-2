6c6a7c6db7b5a3ba77db73bea55dce04aa874c3b
Steven Weaver <sweaver@temple.edu>
2020-04-26 07:46:31 -0400

    2020-04-25

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,12 +2,12 @@
 fdate=$(date +"%Y-%m-%d")
 #fdate="2020-04-14"
 
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1b" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate S" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate M" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate N" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF3a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF7a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF8" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh 
-qsub -d `pwd` -q epyc -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF6" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF1b" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate S" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate M" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate N" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF3a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF7a" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF8" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh 
+qsub -d `pwd` -q epyc2 -F "/data/shares/veg/SARS-CoV-2/SARS-CoV-2/data/fasta/$fdate ORF6" /data/shares/veg/SARS-CoV-2/SARS-CoV-2/scripts/extract_genes.sh
