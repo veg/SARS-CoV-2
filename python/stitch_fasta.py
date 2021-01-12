44a05aa035745c472b5c7e15daea566bcd75df4b
Steven Weaver <sweaver@temple.edu>
2021-01-12 11:47:13 -0500

    updates

--- python/stitch_fasta.py
+++ python/stitch_fasta.py
@@ -18,41 +18,117 @@ import datetime
 import os
 import math, csv
 from   os import  path
-from   Bio import SeqIO
 import operator
+import compress_json
+import mappy
+from progress.bar import Bar
+from   Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+arguments = argparse.ArgumentParser(description='Combine alignments into a single file, adding a reference sequence as well')
+
+arguments.add_argument('-d', '--dir',          help = 'Directory with files',                   required = True, type = str )
+arguments.add_argument('-o', '--output',       help = 'Write the file to',                      required = True, type = argparse.FileType('w'))
+arguments.add_argument('-x', '--extras',       help = 'Insert additional sequences',            required = False, type = argparse.FileType('r'))
+arguments.add_argument('-r', '--reference',    help = 'Reference sequence index',            required = True, type = str)
+arguments.add_argument('-c', '--cutoff',       help = 'Only report variants reaching this count (if >= 1 or frequency if in [0-1])', required = False, type = float, default = 5)
+arguments.add_argument('-m', '--map',          help = 'EPI ID => UID map',            required = True, type = str)
 
 
-arguments = argparse.ArgumentParser(description='Summarize selection analysis results.')
+genes = ['leader','nsp2','nsp3','nsp4','3C','nsp6','nsp7','nsp8','nsp9','nsp10','RdRp','helicase','exonuclease','endornase','methyltransferase','S','ORF3a','E','M','ORF6','ORF7a','ORF8','N','ORF10']
 
-arguments.add_argument('-d', '--dir',        help = 'Directory with files',    required = True, type = str )
-#arguments.add_argument('-d', '--duplicates',   help = 'Duplicate JSON files',      required = True, type = argparse.FileType('r'), nargs = '*')
-arguments.add_argument('-o', '--output',       help = 'Write the file to',         required = True, type = argparse.FileType('w'))
+def load_json_or_compressed (file_name):
+    if file_name.find (".json") + 5 == len(file_name):
+        with open (file_name, "r") as file_object:
+            return json.load (file_object)
+    else:
+        return compress_json.load (file_name)
 
-genes= ['leader', 'nsp2', 'nsp3', 'nsp4', '3C', 'nsp6', 'nsp7', 'nsp8', 'nsp9', 'nsp10', 'RdRp', 'helicase', 'exonuclease', 'endornase','S','ORF3a','M','ORF6','ORF7a','ORF8','N']
+def pad_sequence (s, insert_gap):
+    seq = []
+    si = 0
+    for k in insert_gap:
+        if k:
+            seq.append ('-')
+        else:
+            seq.append (s[si])
+            si += 1
+
+    return ''.join (seq)
+
 
 import_settings = arguments.parse_args()
 
-combined_fasta  = {}
+combined_fasta = {}
+
+
+'''
+#compute alignment consensus
+'''
+
+extras = []
+
+if import_settings.extras:
+    for seq_record in SeqIO.parse(import_settings.extras, "fasta"):
+        extras.append (seq_record)
+
+def consensus_filter (res):
+    if res == '-':
+        return 'N'
+    return res
+
+mapper_r = load_json_or_compressed (import_settings.map)
+mapper = {}
+for i, id in enumerate (mapper_r):
+    mapper [id.split ('_')[2]] = i
+
+consensus = []
+ci = 0
 
 for i, gene in enumerate (genes):
     local_set = {}
 
-    with open (path.join (import_settings.dir, "sequences.%s.duplicates.json" % gene), "r") as dh:
-        dups = json.load (dh)
+    tmp_fn = "".join ([import_settings.dir, "sequences.%s.duplicates.json" % gene])
+
+    if(path.exists(tmp_fn)):
+        dup_fn = tmp_fn
+    else:
+        dup_fn = tmp_fn + '.gz'
+
+    try:
+       dups = load_json_or_compressed (dup_fn)
+    except FileNotFoundError as e:
+       print("duplicate file not found: " + dup_fn + ", trying another name")
+       continue
 
     count = 0
 
-    with open (path.join (import_settings.dir, "sequences.%s.compressed.fas" % gene), "r") as fasta:
-        for seq_record in SeqIO.parse(fasta, "fasta"):
-            seq_id   = seq_record.name
-            seq = str (seq_record.seq)
-            no_count = '_'.join (seq_id.split ('_')[0:-1])
-            local_set [no_count] = seq
-            count += len (dups[seq_id])
-            for i,dup_id in  dups[seq_id].items():
-                if dup_id != seq_id:
-                    local_set [dup_id] = seq
+    for seq_record in SeqIO.parse(open ("".join ([import_settings.dir, "sequences.%s.compressed.fas" % gene]), "r"), "fasta"):
+        seq_id   = seq_record.name
+        seq = str (seq_record.seq).upper()
+        no_count = mapper[seq_id.split ('_')[2]]
+        local_set [no_count] = seq
+        _copy_count = len (dups[seq_id])
+        count += len (dups[seq_id])
+        #print (dups[seq_id], file = sys.stderr)
+        for i,dup_id in  dups[seq_id].items():
+            dup_id_clean = mapper[dup_id.split ('_')[2]]
+            if dup_id_clean != seq_id:
+                local_set [dup_id_clean] = seq
 
+        if ci == len (consensus):
+            for l in enumerate (seq):
+                consensus.append ({})
+            #consensus = [{} for l in enumerate (seq)]
+
+        for i,l in enumerate (seq):
+            if l not in consensus[ci+i]:
+                consensus[ci+i][l] = _copy_count
+            else:
+                consensus[ci+i][l] += _copy_count
+
+    ci = len (consensus)
 
     if len (combined_fasta):
         to_delete = set ()
@@ -69,8 +145,104 @@ for i, gene in enumerate (genes):
         for i,s in local_set.items():
             combined_fasta[i] = s
 
-    print (len (combined_fasta), count)
-    #sys.exit (0)
+    print (gene, len (combined_fasta), count, file = sys.stderr)
 
-for i, s in combined_fasta.items():
-    print (">%s\n%s\n" % (i,s), file = import_settings.output)
+ref_seq = ''.join ([consensus_filter(max(pos.items(), key=operator.itemgetter(1))[0]) for pos in consensus ])
+
+aligner     = mappy.Aligner (import_settings.reference, preset = "asm5")
+genome      = aligner.seq (aligner.seq_names[0])
+
+ref_seq_output  = []
+
+
+ref = 0
+qry = 0
+
+summary_json    = {
+                        'reference_base' : [],
+                        'sequences' : {}
+                  }
+
+aligner_segments = sorted (list (aligner.map (ref_seq)), key=lambda x: x.r_st)
+
+
+for i, segment in enumerate (aligner_segments):
+    print (ref, qry, segment.r_st , segment.q_st, segment.r_en, segment.q_en)
+    while ref < segment.r_st :
+        summary_json['reference_base'].append (genome[ref])
+        ref += 1
+    while qry < segment.q_st:
+        #ref_seq_output.append ([-1,'-'])
+        ref_seq_output.append ([-ref-1,'-'])
+        qry += 1
+
+    if i > 0:
+        print (genome[last_endr:segment.r_st])
+        print (ref_seq[last_endq:segment.q_st])
+
+    last_endq =      segment.q_en
+    last_endr =      segment.r_en
+
+    for op in segment.cigar:
+        if op[1] == 0: # Matcher
+            for i in range (op[0]):
+                #print (genome[ref],":",ref_seq[qry])
+                ref_seq_output.append ([ref,genome[ref]])
+                summary_json['reference_base'].append (genome[ref])
+                ref+=1
+                qry+=1
+        elif op[1] == 1: #insertion
+            for i in range (op[0]):
+                #print ("-:",ref_seq[qry])
+                ref_seq_output.append ([-ref-1,'-'])
+                qry+=1
+        elif op[1] == 2: #deletion
+            for i in range (op[0]):
+                #ref_seq_output.append ([ref,genome[ref]])
+                summary_json['reference_base'].append (genome[ref])
+                #print (genome[ref],":-")
+                ref+=1
+
+
+for i in range (qry, len (ref_seq)): #tail end of the consensus did not map to anything
+    ref_seq_output.append ([-ref-1,'-'])
+
+variant_counts  = {}
+sequence_count  = 0
+
+with Bar('Calling variants in sequences', max=len (combined_fasta)) as bar:
+    for seq_id, seq in combined_fasta.items ():
+
+        seq = seq.upper()
+        sequence_count += 1
+        summary_json ['sequences'][seq_id] = [(ref_seq_output[k][0],seq[k]) for k in range (len (seq)) if ref_seq_output[k][0]>=0 and ref_seq_output[k][1] != seq[k] and seq[k] != '-']
+
+
+        for variant in summary_json ['sequences'][seq_id]:
+            if not variant in variant_counts:
+                variant_counts [variant] = 0
+            variant_counts [variant] += 1
+
+        bar.next()
+        #print (seq_id, summary_json ['sequences'] [seq_id], file = sys.stderr)
+
+if import_settings.cutoff < 1.:
+    import_settings.cutoff *= sequence_count
+
+accepted_variants = {}
+for v,c in variant_counts.items():
+    if c >= import_settings.cutoff:
+        accepted_variants[v] = c
+
+
+summary_json['variants'] = [[k[0],k[1],c] for k, c in accepted_variants.items()]
+
+filtered_variants = {}
+for s, variants in summary_json ['sequences'].items():
+    filtered_variants[s] = [l for k in variants if k in accepted_variants for l in k]
+
+summary_json ['sequences'] = filtered_variants
+
+json.dump (summary_json, import_settings.output, indent = 1)
+
+
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,43 +1,45 @@
 #!/bin/bash
 #fdate=$(date +"%Y-%m-%d")
-#fdate=$1
-fdate="2020-12-22"
-#fdate="2020-10-12"
+fdate=$1
+#fdate="2021-01-07"
 
-FQUEUE='epyc'
+FQUEUE='epyc2'
 QUEUE='epyc'
 OUTPUT_DIR=`pwd`/logs/$fdate/
 #BASE_DIR="/data/shares/veg/SARS-CoV-2/SARS-CoV-2-devel"
 BASE_DIR=`pwd`
 mkdir $OUTPUT_DIR
 
+SMALLPPN=16
+LARGEPPN=64
+
 # Gene
-qsub -l nodes=1:ppn=16 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $FQUEUE -F "$BASE_DIR/data/fasta/$fdate S 16" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate M 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate N 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate E 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF3a 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF6 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF7a 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF7b 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF8 8" $BASE_DIR/scripts/extract_genes.sh 
+qsub -l nodes=1:ppn=$LARGEPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $FQUEUE -F "$BASE_DIR/data/fasta/$fdate S $LARGEPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate M $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate N $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate E $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF3a $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF6 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF7a $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF7b $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF8 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh 
 #qsub -l nodes=2:ppn=64 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF1a 128" $BASE_DIR/scripts/extract_genes.sh
 #qsub -l nodes=2:ppn=64 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIRkdata/fasta/$fdate ORF1b 128" $BASE_DIR/scripts/extract_genes.sh
 
 # Products
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate leader 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp2 8" $BASE_DIR/scripts/extract_genes.sh
-#qsub -l nodes=1:ppn=16 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $FQUEUE -F "$BASE_DIR/data/fasta/$fdate nsp3 16" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp4 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate 3C 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp6 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp7 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp8 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp9 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp10 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate RdRp 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate helicase 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate exonuclease 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate endornase 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate methyltransferase 8" $BASE_DIR/scripts/extract_genes.sh
-qsub -l nodes=1:ppn=8 -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF10 8" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate leader $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp2 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$LARGEPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $FQUEUE -F "$BASE_DIR/data/fasta/$fdate nsp3 $LARGEPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp4 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate 3C $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp6 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp7 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp8 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp9 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate nsp10 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate RdRp $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate helicase $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate exonuclease $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate endornase $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate methyltransferase $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
+qsub -l nodes=1:ppn=$SMALLPPN -d `pwd` -o $OUTPUT_DIR -e $OUTPUT_DIR -q $QUEUE -F "$BASE_DIR/data/fasta/$fdate ORF10 $SMALLPPN" $BASE_DIR/scripts/extract_genes.sh
