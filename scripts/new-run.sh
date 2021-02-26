b7275869fe0e667200a4135889e2b0766ea1ab15
Steven Weaver <sweaver@temple.edu>
2021-02-25 20:14:21 -0800

    Delete new-run.sh

--- scripts/new-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-DATE=$(date +"%Y-%m-%d")
-mkdir data/fasta/$DATE
-python3 python/export-sequences.py -o data/fasta/$DATE/sequences
-python3 python/export-meta.py -o data/fasta/$DATE/master-no-fasta.json
-./submit_jobs.sh $DATE
