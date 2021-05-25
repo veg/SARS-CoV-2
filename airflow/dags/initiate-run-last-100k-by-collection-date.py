f068b021882284cadf8c4f7cd1b5d0e6a43a78df
Steven Weaver <sweaver@temple.edu>
2021-05-25 19:26:33 -0400

    fix last 100k

--- airflow/dags/initiate-run-last-100k-by-collection-date.py
+++ airflow/dags/initiate-run-last-100k-by-collection-date.py
@@ -189,134 +189,134 @@ with DAG(
 
         export_bealign_task >> cleanup_task
 
-        with TaskGroup(f"duplicates_{gene}") as duplicates_group:
+    with TaskGroup(f"duplicates_{gene}") as duplicates_group:
 
-            compute_duplicates_task = PythonOperator(
-                task_id=f'write_raw_duplicates',
-                python_callable=write_nuc_raw_duplicates,
-                op_kwargs={ "input" : nuc_sequence_output, "duplicate_output" : duplicate_output, 'uniques_output': uniques_fn },
-                dag=dag,
-            )
+        compute_duplicates_task = PythonOperator(
+            task_id=f'write_raw_duplicates',
+            python_callable=write_nuc_raw_duplicates,
+            op_kwargs={ "input" : nuc_sequence_output, "duplicate_output" : duplicate_output, 'uniques_output': uniques_fn },
+            dag=dag,
+        )
 
-            compute_duplicates_task
+        compute_duplicates_task
 
-        with TaskGroup(f"filter_{gene}") as filter:
-            COMPRESSOR = """
-            {{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} {{ params.compressor }} --msa $FASTA_FN --regexp "epi_isl_([0-9]+)" --duplicates $DUPLICATE_FN --output $VARIANTS_CSV_FN  --json $VARIANTS_JSON_FN --duplicate-out $COMPRESSOR_DUPLICATE_OUT
-            """
-            compressor_task = BashOperator(
-                task_id=f'compressor',
-                bash_command=COMPRESSOR,
-                env={'FASTA_FN': uniques_fn, 'DUPLICATE_FN': duplicate_output, 'VARIANTS_CSV_FN': variants_csv_output, 'VARIANTS_JSON_FN': variants_json_output, 'COMPRESSOR_DUPLICATE_OUT': compressor_duplicate_out, **os.environ},
-                dag=dag
-            )
-
-            # --output-edits ${FILE}.${GENE}.filtered.edits.json
-            COMPRESSOR2 = """
-            {{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} {{ params.compressor2 }} --msa $FASTA_FN --duplicates $DUPLICATE_FN --csv $VARIANTS_CSV_FN  --byseq $VARIANTS_JSON_FN --p 0.95 --output $FILTERED_FASTA_FN --json $FILTERED_JSON_FN --output-edits ${OUTPUT_EDITS}
-            """
-            compressor_two_task = BashOperator(
-                task_id=f'compressor_two',
-                bash_command=COMPRESSOR2,
-                env={'FASTA_FN': uniques_fn, 'DUPLICATE_FN': compressor_duplicate_out, 'VARIANTS_CSV_FN': variants_csv_output, 'VARIANTS_JSON_FN': variants_json_output, 'FILTERED_FASTA_FN': filtered_fasta_output, 'FILTERED_JSON_FN': filtered_json_output, 'OUTPUT_EDITS': output_edits_fn, **os.environ},
-                dag=dag
-            )
-
-            # bpsh 0 tn93-cluster -t 0.001 sequences.S.compressed.filtered.fas
-            TN93_CLUSTER = """
-            {{ params.tn93_cluster }} -t {{ params.threshold }} -o {{ params.output_fn }} {{ params.input_fn }}
-            """
-            tn93_cluster_task = BashOperator(
-                task_id=f'tn93_cluster',
-                bash_command=TN93_CLUSTER,
-                params={'input_fn': filtered_fasta_output, 'threshold': str(regions[gene]['cluster_threshold']), 'output_fn': tn93_cluster_fn},
-                dag=dag
-            )
-
-            WRITE_CENTROIDS= """
-            cat {{ params.input_fn }} | /usr/local/bin/jq -r '.[].centroid' > {{ params.output_fn }}
-            """
-            write_centroids_task = BashOperator(
-                task_id=f'write_centroids',
-                bash_command=WRITE_CENTROIDS,
-                params={ 'input_fn': tn93_cluster_fn, 'output_fn': centroid_fn },
-                dag=dag
-            )
-
-
-            compressor_task >> compressor_two_task >> tn93_cluster_task >> write_centroids_task
-
-        INFER_TREE = """
-        seqmagick convert $FILTERED_FASTA_FN $STO_OUTPUT;
-        rapidnj $STO_OUTPUT -i sth > $TREE_OUTPUT
-        sed -i "s/'//g" $TREE_OUTPUT;
+    with TaskGroup(f"filter_{gene}") as filter:
+        COMPRESSOR = """
+        {{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} {{ params.compressor }} --msa $FASTA_FN --regexp "epi_isl_([0-9]+)" --duplicates $DUPLICATE_FN --output $VARIANTS_CSV_FN  --json $VARIANTS_JSON_FN --duplicate-out $COMPRESSOR_DUPLICATE_OUT
         """
-
-        infer_tree_task = BashOperator(
-            task_id=f'infer_tree_{gene}',
-            bash_command=INFER_TREE,
-            env={'FILTERED_FASTA_FN': centroid_fn, 'STO_OUTPUT': sto_output, 'TREE_OUTPUT': tree_output, **os.environ},
+        compressor_task = BashOperator(
+            task_id=f'compressor',
+            bash_command=COMPRESSOR,
+            env={'FASTA_FN': uniques_fn, 'DUPLICATE_FN': duplicate_output, 'VARIANTS_CSV_FN': variants_csv_output, 'VARIANTS_JSON_FN': variants_json_output, 'COMPRESSOR_DUPLICATE_OUT': compressor_duplicate_out, **os.environ},
             dag=dag
         )
 
-        slac_task = BashOperator(
-            task_id=f'slac_{gene}',
-            bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} slac --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches All --samples 0 --output $SLAC_OUTPUT",
-            env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'SLAC_OUTPUT': slac_output_fn, **os.environ},
-            pool='hyphy',
-            dag=dag,
-        )
-
-        big_data_flags='--full-model No'
-
-        fel_task = BashOperator(
-            task_id=f'fel_{gene}',
-            bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} fel --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' $BIG_DATA_FLAGS --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches Internal --output $FEL_OUTPUT",
-            env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'FEL_OUTPUT': fel_output_fn, 'BIG_DATA_FLAGS': big_data_flags, **os.environ},
-            pool='hyphy',
-            dag=dag,
-        )
-
-        meme_task = BashOperator(
-            task_id=f'meme_{gene}',
-            bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} meme --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' $BIG_DATA_FLAGS --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches Internal --output $MEME_OUTPUT",
-            env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'MEME_OUTPUT': meme_output_fn, 'BIG_DATA_FLAGS': big_data_flags, **os.environ},
-            pool='hyphy',
-            dag=dag,
-        )
-
-        annotation_file = filepath_prefix + '.annotation.json'
-        copy_annotation_task = BashOperator(
-            task_id=f'copy_annotation_{gene}',
-            bash_command='cp {{params.working_dir}}/data/comparative-annotation.json {{params.annotation_file}}',
-            params={'annotation_file': annotation_file, 'working_dir': WORKING_DIR},
+        # --output-edits ${FILE}.${GENE}.filtered.edits.json
+        COMPRESSOR2 = """
+        {{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} {{ params.compressor2 }} --msa $FASTA_FN --duplicates $DUPLICATE_FN --csv $VARIANTS_CSV_FN  --byseq $VARIANTS_JSON_FN --p 0.95 --output $FILTERED_FASTA_FN --json $FILTERED_JSON_FN --output-edits ${OUTPUT_EDITS}
+        """
+        compressor_two_task = BashOperator(
+            task_id=f'compressor_two',
+            bash_command=COMPRESSOR2,
+            env={'FASTA_FN': uniques_fn, 'DUPLICATE_FN': compressor_duplicate_out, 'VARIANTS_CSV_FN': variants_csv_output, 'VARIANTS_JSON_FN': variants_json_output, 'FILTERED_FASTA_FN': filtered_fasta_output, 'FILTERED_JSON_FN': filtered_json_output, 'OUTPUT_EDITS': output_edits_fn, **os.environ},
             dag=dag
         )
 
-        summarize_gene_task = BashOperator(
-            task_id=f'summarize_gene_{gene}',
-            bash_command='{{ params.python }} {{params.working_dir}}/python/summarize_gene.py -T {{params.working_dir}}/data/ctl/epitopes.json -B {{params.working_dir}}/data/single_mut_effects.csv -D $MASTERNOFASTA -d $DUPLICATES -s $SLAC_OUTPUT -f $FEL_OUTPUT -m $MEME_OUTPUT -P 0.1 --output  $SUMMARY_OUTPUT -c $COMPRESSED_OUTPUT_FN -E {{params.working_dir}}/data/evo_annotation.json -A {{params.working_dir}}/data/mafs.csv -V {{params.working_dir}}/data/evo_freqs.csv -F $FRAGMENT --frame_shift $ADDSHIFT --fragment_shift $SHIFT -S $OFFSET -O $ANNOTATION',
-            params={'python': default_args['params']['python'], 'working_dir': WORKING_DIR},
-            env={
-                'MASTERNOFASTA': default_args["params"]["meta-output"],
-                'DUPLICATES': duplicate_output,
-                'SLAC_OUTPUT': slac_output_fn,
-                'FEL_OUTPUT': fel_output_fn,
-                'MEME_OUTPUT': meme_output_fn,
-                'SUMMARY_OUTPUT': summary_output_fn,
-                'COMPRESSED_OUTPUT_FN': filtered_fasta_output,
-                'FRAGMENT': str(regions[gene]['fragment']),
-                'ADDSHIFT': str(regions[gene]['add_one']),
-                'SHIFT': str(regions[gene]['shift']),
-                'OFFSET': str(regions[gene]['offset']),
-                'ANNOTATION': annotation_file,
-                **os.environ},
-            dag=dag,
+        # bpsh 0 tn93-cluster -t 0.001 sequences.S.compressed.filtered.fas
+        TN93_CLUSTER = """
+        {{ params.tn93_cluster }} -t {{ params.threshold }} -o {{ params.output_fn }} {{ params.input_fn }}
+        """
+        tn93_cluster_task = BashOperator(
+            task_id=f'tn93_cluster',
+            bash_command=TN93_CLUSTER,
+            params={'input_fn': filtered_fasta_output, 'threshold': str(regions[gene]['cluster_threshold']), 'output_fn': tn93_cluster_fn},
+            dag=dag
         )
 
-        summarize_gene_task.set_upstream(export_meta_task)
-        alignment.set_upstream(export_sequences_task)
-        export_by_gene.append(alignment >> duplicates_group >> filter >> infer_tree_task >> [slac_task, fel_task, meme_task] >> copy_annotation_task >> summarize_gene_task)
+        WRITE_CENTROIDS= """
+        cat {{ params.input_fn }} | /usr/local/bin/jq -r '.[].centroid' > {{ params.output_fn }}
+        """
+        write_centroids_task = BashOperator(
+            task_id=f'write_centroids',
+            bash_command=WRITE_CENTROIDS,
+            params={ 'input_fn': tn93_cluster_fn, 'output_fn': centroid_fn },
+            dag=dag
+        )
+
+
+        compressor_task >> compressor_two_task >> tn93_cluster_task >> write_centroids_task
+
+    INFER_TREE = """
+    seqmagick convert $FILTERED_FASTA_FN $STO_OUTPUT;
+    rapidnj $STO_OUTPUT -i sth > $TREE_OUTPUT
+    sed -i "s/'//g" $TREE_OUTPUT;
+    """
+
+    infer_tree_task = BashOperator(
+        task_id=f'infer_tree_{gene}',
+        bash_command=INFER_TREE,
+        env={'FILTERED_FASTA_FN': centroid_fn, 'STO_OUTPUT': sto_output, 'TREE_OUTPUT': tree_output, **os.environ},
+        dag=dag
+    )
+
+    slac_task = BashOperator(
+        task_id=f'slac_{gene}',
+        bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} slac --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches All --samples 0 --output $SLAC_OUTPUT",
+        env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'SLAC_OUTPUT': slac_output_fn, **os.environ},
+        pool='hyphy',
+        dag=dag,
+    )
+
+    big_data_flags='--full-model No'
+
+    fel_task = BashOperator(
+        task_id=f'fel_{gene}',
+        bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} fel --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' $BIG_DATA_FLAGS --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches Internal --output $FEL_OUTPUT",
+        env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'FEL_OUTPUT': fel_output_fn, 'BIG_DATA_FLAGS': big_data_flags, **os.environ},
+        pool='hyphy',
+        dag=dag,
+    )
+
+    meme_task = BashOperator(
+        task_id=f'meme_{gene}',
+        bash_command="{{ params.hyphy }} LIBPATH={{params.hyphy_lib_path}} meme --kill-zero-lengths Constrain ENV='_DO_TREE_REBALANCE_=1' $BIG_DATA_FLAGS --alignment $FILTERED_FASTA_FN --tree $TREE_OUTPUT --branches Internal --output $MEME_OUTPUT",
+        env={'FILTERED_FASTA_FN': centroid_fn, 'TREE_OUTPUT': tree_output, 'MEME_OUTPUT': meme_output_fn, 'BIG_DATA_FLAGS': big_data_flags, **os.environ},
+        pool='hyphy',
+        dag=dag,
+    )
+
+    annotation_file = filepath_prefix + '.annotation.json'
+    copy_annotation_task = BashOperator(
+        task_id=f'copy_annotation_{gene}',
+        bash_command='cp {{params.working_dir}}/data/comparative-annotation.json {{params.annotation_file}}',
+        params={'annotation_file': annotation_file, 'working_dir': WORKING_DIR},
+        dag=dag
+    )
+
+    summarize_gene_task = BashOperator(
+        task_id=f'summarize_gene_{gene}',
+        bash_command='{{ params.python }} {{params.working_dir}}/python/summarize_gene.py -T {{params.working_dir}}/data/ctl/epitopes.json -B {{params.working_dir}}/data/single_mut_effects.csv -D $MASTERNOFASTA -d $DUPLICATES -s $SLAC_OUTPUT -f $FEL_OUTPUT -m $MEME_OUTPUT -P 0.1 --output  $SUMMARY_OUTPUT -c $COMPRESSED_OUTPUT_FN -E {{params.working_dir}}/data/evo_annotation.json -A {{params.working_dir}}/data/mafs.csv -V {{params.working_dir}}/data/evo_freqs.csv -F $FRAGMENT --frame_shift $ADDSHIFT --fragment_shift $SHIFT -S $OFFSET -O $ANNOTATION',
+        params={'python': default_args['params']['python'], 'working_dir': WORKING_DIR},
+        env={
+            'MASTERNOFASTA': default_args["params"]["meta-output"],
+            'DUPLICATES': duplicate_output,
+            'SLAC_OUTPUT': slac_output_fn,
+            'FEL_OUTPUT': fel_output_fn,
+            'MEME_OUTPUT': meme_output_fn,
+            'SUMMARY_OUTPUT': summary_output_fn,
+            'COMPRESSED_OUTPUT_FN': filtered_fasta_output,
+            'FRAGMENT': str(regions[gene]['fragment']),
+            'ADDSHIFT': str(regions[gene]['add_one']),
+            'SHIFT': str(regions[gene]['shift']),
+            'OFFSET': str(regions[gene]['offset']),
+            'ANNOTATION': annotation_file,
+            **os.environ},
+        dag=dag,
+    )
+
+    summarize_gene_task.set_upstream(export_meta_task)
+    alignment.set_upstream(export_sequences_task)
+    export_by_gene.append(alignment >> duplicates_group >> filter >> infer_tree_task >> [slac_task, fel_task, meme_task] >> copy_annotation_task >> summarize_gene_task)
 
     dag.doc_md = __doc__
 
