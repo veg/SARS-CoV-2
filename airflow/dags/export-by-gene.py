84241a5742dd957191faa5f8b13d14ba5fc9ec82
Steven Weaver <sweaver@temple.edu>
2021-06-02 18:06:44 -0400

    export all genes

--- airflow/dags/export-by-gene.py
+++ airflow/dags/export-by-gene.py
@@ -6,6 +6,7 @@ from airflow import DAG
 
 # Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
+from airflow.models.baseoperator import cross_downstream
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.hooks.base import BaseHook
@@ -20,7 +21,7 @@ p = os.path.abspath(str(pathlib.Path(__file__).parent.absolute()) + '/../../pyth
 if p not in sys.path:
     sys.path.append(p)
 
-from export_sequences import export_sequences, export_premsa_sequences
+from export_sequences import export_sequences, export_premsa_sequences, export_bealign_sequences
 from export_duplicates import export_duplicates
 from export_meta import export_meta
 
@@ -42,83 +43,100 @@ default_args = {
     },
     'retries': 1,
     'retry_delay': datetime.timedelta(minutes=5),
-    'concurrency' : 50,
-    'on_failure_callback': task_fail_slack_alert,
-    # 'on_success_callback': task_success_slack_alert
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
+    'concurrency' : 50
 }
 
-gene = default_args["params"]["gene"]
-directory_output = WORKING_DIR + "/data/" + gene + "/" + default_args["params"]["date"]
 
-default_args["params"]["directory"] = directory_output
-default_args["params"]["sequence-output"] = directory_output + '/sequences'
-default_args["params"]["meta-output"] = directory_output  + '/master-no-sequences.json'
-default_args["params"]["duplicate-output"] = directory_output  + '/duplicates.json'
-
-nuc_sequence_output = default_args["params"]["directory"] + '/sequences_nuc.fas'
-prot_sequence_output = default_args["params"]["directory"] + '/sequences_protein.fas'
-
-default_args["params"]["nuc-sequence-output"] = nuc_sequence_output
-default_args["params"]["prot-sequence-output"] = prot_sequence_output
-
-dag = DAG(
+with DAG(
     'export_by_gene',
     default_args=default_args,
     description='export_by_gene',
     schedule_interval='@weekly',
-    start_date=datetime.datetime(2021, 2, 25),
+    start_date=datetime.datetime(2021, 6, 2),
     tags=['selection'],
-)
+    ) as dag:
 
-with open(dag.params["region_cfg"], 'r') as stream:
-    regions = yaml.safe_load(stream)
+    with open(dag.params["region_cfg"], 'r') as stream:
+        regions = yaml.safe_load(stream)
 
-mk_dir_task = BashOperator(
-    task_id='make_directory',
-    bash_command='mkdir -p {{params.directory}}',
-    dag=dag,
-)
-
-export_meta_task = PythonOperator(
-        task_id='export_meta',
-        python_callable=export_meta,
-        op_kwargs={ "config" : default_args['params'] },
+    mk_dir_task = BashOperator(
+        task_id='make_directory',
+        bash_command='mkdir -p {{params.directory}}',
         dag=dag,
     )
 
-export_duplicates_task = PythonOperator(
-        task_id=f'export_duplicates_{default_args["params"]["gene"]}',
-        python_callable=export_duplicates,
-        op_kwargs={ 'output_fn' : default_args['params']['duplicate-output'], 'gene': default_args['params']['gene'] },
-        dag=dag,
-    )
+    export_meta_task = PythonOperator(
+            task_id='export_meta',
+            python_callable=export_meta,
+            op_kwargs={ "config" : default_args['params'] },
+            pool='mongo',
+            dag=dag,
+        )
 
-# For each region
-export_by_gene_task = PythonOperator(
-        task_id=f'export_premsa_sequences_{default_args["params"]["gene"]}',
-        python_callable=export_premsa_sequences,
-        op_kwargs={ "config" : default_args['params'], 'nuc_output_fn':  nuc_sequence_output, 'prot_output_fn' : prot_sequence_output, 'gene' : gene },
-        dag=dag,
-    )
+    export_meta_task.set_upstream(mk_dir_task)
 
-dag.doc_md = __doc__
+    last_exec_date = dag.get_latest_execution_date()
 
-export_by_gene_task.doc_md = """\
-# Task Documentation
-Exports by specific gene
-"""
+    if last_exec_date is None:
+        last_exec_date = datetime.datetime(year=1970, month=1, day=1)
 
-# Add export meta and export sequence tasks to be executed in parallel
-mk_dir_task >> [export_meta_task, export_duplicates_task, export_by_gene_task]
+    unique_id = str(round(last_exec_date.timestamp()))
+
+    export_by_gene = []
+
+    for gene in regions.keys():
+
+        directory_output = WORKING_DIR + "/data/" + gene + "/" + unique_id
+
+        default_args["params"]["directory"] = directory_output
+        default_args["params"]["sequence-output"] = directory_output + '/sequences'
+        default_args["params"]["meta-output"] = directory_output  + '/master-no-sequences.json'
+        default_args["params"]["duplicate-output"] = directory_output  + '/duplicates.json'
+
+        nuc_sequence_output = default_args["params"]["directory"] + '/sequences_nuc.fas'
+        bealign_nuc_sequence_output = default_args["params"]["directory"] + '/sequences_nuc.bealign.fas'
+        prot_sequence_output = default_args["params"]["directory"] + '/sequences_protein.fas'
+
+        default_args["params"]["nuc-sequence-output"] = nuc_sequence_output
+        default_args["params"]["prot-sequence-output"] = prot_sequence_output
+
+        export_duplicates_task = PythonOperator(
+                task_id=f'export_duplicates_{gene}',
+                python_callable=export_duplicates,
+                op_kwargs={ 'output_fn' : default_args['params']['duplicate-output'], 'gene': default_args['params']['gene'] },
+                pool='mongo',
+                dag=dag,
+            )
+
+        # For each region
+        export_by_gene_task = PythonOperator(
+                task_id=f'export_premsa_sequences_{gene}',
+                python_callable=export_premsa_sequences,
+                op_kwargs={ "config" : default_args['params'], 'nuc_output_fn':  nuc_sequence_output, 'prot_output_fn' : prot_sequence_output, 'gene' : gene },
+                pool='mongo',
+                dag=dag,
+            )
+
+        export_bealign_task = PythonOperator(
+            task_id=f'export_bealign_{gene}',
+            python_callable=export_bealign_sequences,
+            op_kwargs={ "config" : default_args['params'], 'nuc_output_fn':  bealign_nuc_sequence_output, 'gene' : gene },
+            provide_context=True,
+            pool='mongo',
+            dag=dag,
+        )
+
+
+        export_by_gene.extend([export_duplicates_task, export_by_gene_task, export_bealign_sequences])
+
+    dag.doc_md = __doc__
+
+    export_by_gene_task.doc_md = """\
+        # Task Documentation
+        Exports by specific gene
+    """
+
+    # Add export meta and export sequence tasks to be executed in parallel
+    export_meta_task >> export_by_gene
+
 
