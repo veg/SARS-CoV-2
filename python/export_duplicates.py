14176c16e22e31ffb6754093366dc774a7c24c62
Steven Weaver <sweaver@temple.edu>
2021-08-24 19:00:42 -0400

    dupe

--- python/export_duplicates.py
+++ python/export_duplicates.py
@@ -8,12 +8,13 @@ import copy
 import os
 import multiprocessing
 from multiprocessing import Pool
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from operator import itemgetter
 from Bio import SeqIO
 
 import pymongo
 from pymongo import MongoClient
+from bson.son import SON
 
 def get_duplicates(records, gene):
     references = {}
@@ -40,25 +41,38 @@ def get_duplicates(records, gene):
 def export_duplicates(output_fn, gene):
 
     db = MongoClient(host='192.168.0.4')
-    acceptable = ['id', 'duplicate_of_by_gene']
 
-    # transform acceptable into mongo query
-    acceptable_dict = { k: 1 for k in acceptable}
+    start_date='2020-06-01'
+    end_date='2021-09-01'
 
-    HOST= "Human"
-    MINLENGTH=28000
-    validation_key = 'qc.' + gene + '.passed'
-    mongo_query = { "host" : HOST,  "length": {"$gt": MINLENGTH }, "seq": {"$exists":True}, validation_key: True }
+    aggregate_query = [
+        { "$match": { "submitted": { "$gte": datetime.strptime(start_date, "%Y-%m-%d"), "$lte": datetime.strptime(end_date, "%Y-%m-%d") } } },
+        { "$group" : { "_id": {"S_premsa_nuc_seq": "$S_premsa_nuc_seq"}, "uniqueIds": {"$addToSet": "$id"}, "count": {"$sum": 1 } } },
+        { "$match": { "count": {"$gt": 1} } },
+        { "$sort": { "count" : -1 } },
+        { "$merge": {
+           "into": "dev_duplicate_agg",
+           "on" : "_id",
+           "whenMatched": [ { "$set": {
+              "uniqueIds": { "$setUnion": ["$uniqueIds", "$$new.uniqueIds"] },
+              "count": { "$size": { "$setUnion": ["$uniqueIds", "$$new.uniqueIds"] } },
+           } } ],
+           "whenNotMatched": "insert"
+       }}
+    ]
 
-    records = list(db.gisaid.records.find(mongo_query, acceptable_dict))
-    records = [{k: v for k, v in rec.items() if k in acceptable} for rec in records]
+    # records = list(db.gisaid.dev.aggregate(aggregate_query, aggregate_opts))
+    records = db.gisaid.dev.aggregate(aggregate_query, allowDiskUse=True)
+    # import pdb;pdb.set_trace()
+    records = [{k: v for k, v in rec.items()} for rec in records]
+    print(records)
 
-    # split items with duplicate and those that have reference/missing
-    duplicates = get_duplicates(records, gene)
+    # Now, instead of getting the entire collection, let's loop through submit dates and merge into dev_duplicates
+
 
     # Write attributes.csv file and MASTER-NO-JSON file
     with open(output_fn, 'w') as output_fh:
-        json.dump(duplicates, output_fh, indent=4, sort_keys=True)
+        json.dump(records, output_fh, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
     arguments = argparse.ArgumentParser(description='Report which dates have full report')
