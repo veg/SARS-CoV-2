bcceed2c87267933e488a0a548600b2dffd5eef3
Steven Weaver <sweaver@temple.edu>
2022-01-03 16:51:42 -0500

    updates

--- airflow/dags/import.py
+++ airflow/dags/import.py
@@ -95,8 +95,8 @@ new_fasta = default_args['params']['import_dir'] + 'new.fasta'
 
 translate_tsv_task = PythonOperator(
     task_id='translate_tsv',
-    python_callable=translate_tsv,
-    op_kwargs={ "tsvfile" : open(tsvfile, 'r'), "outfile" : open(translate_meta, 'w') },
+    python_callable=lambda tsvfile,translate_meta: translate_tsv(open(tsvfile, 'r'), open(translate_meta, 'w')),
+    op_kwargs={ "tsvfile" : tsvfile, "outfile" : translate_meta },
     dag=dag,
     priority_weight=9000
 )
